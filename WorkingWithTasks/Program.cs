7a72b7912ad501613f8fd1954f5869ee0cc3397a
ryan <ryancumming7@gmail.com>
2022-05-26 16:19:50 +0100

    Added method to wait for simulated web call result before continuing with other method using said result

--- WorkingWithTasks/Program.cs
+++ WorkingWithTasks/Program.cs
@@ -29,6 +29,22 @@ namespace WorkingWithTasks
             WriteLine("Finished method C.");
         }
 
+        static decimal SimCallWebService()
+        {
+            WriteLine("Starting simulation to call web service...");
+            Thread.Sleep((new Random()).Next(2000, 4000));
+            WriteLine("Finished call to web service.");
+            return 89.99M;
+        }
+
+        static string CallStoredProcedure(decimal amount)
+        {
+            WriteLine("Starting call to stored procedure...");
+            Thread.Sleep((new Random()).Next(2000, 4000));
+            WriteLine("Finished call to stored procedure.");
+            return $"12 products cost more than {amount:C}.";
+        }
+
         static void Main(string[] args)
         {
             var timer = Stopwatch.StartNew();
@@ -36,13 +52,21 @@ namespace WorkingWithTasks
             //MethodA();
             //MethodB();
             //MethodC();
-            WriteLine("Running methods asynchronously on multiple threads.");
+
+            /*WriteLine("Running methods asynchronously on multiple threads.");
             Task taskA = new Task(MethodA);
             taskA.Start();
             Task taskB = Task.Factory.StartNew(MethodB);
             Task taskC = Task.Run(new Action(MethodC));
             Task[] tasks = { taskA, taskB, taskC }; // Add the newly created tasks to an array
-            Task.WaitAll(tasks); // Wait for all tasks to be completed before continuing
+            Task.WaitAll(tasks); // Wait for all tasks to be completed before continuing*/
+
+            WriteLine("Passing the result of one task as an input into another.");
+            var taskCallWebServiceAndThenStoredProcedure =
+                Task.Factory.StartNew(SimCallWebService)
+                .ContinueWith(previousTask => CallStoredProcedure(previousTask.Result));
+
+            WriteLine($"Result: {taskCallWebServiceAndThenStoredProcedure.Result}");
             WriteLine($"{timer.ElapsedMilliseconds:#,##0}ms elapsed.");
         }
     }
